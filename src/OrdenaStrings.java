6a6ffbe3dd7287b4402ed0b69ad2b849f00cb7ec
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-06-11 08:28:54 -0300

    Java8

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+    public static void main(String[] args) {
+        List<String> palavras = new ArrayList<>();
+        palavras.add("alura online");
+        palavras.add("casa do código");
+        palavras.add("caelum");
+
+        Collections.sort(palavras);
+        System.out.println(palavras);
+
+        //Comparator<String> comparador = new ComparadorDeStringPorTamanho();
+        //Collections.sort(palavras, comparador);
+
+        Comparator<String> comparador = new ComparadorDeStringPorTamanho();
+        palavras.sort(comparador);
+
+        //for(String p : palavras){
+        //    System.out.println(p);
+        //}
+
+        Consumer<String> consumidor = new ImprimenaLinha();
+        palavras.forEach(consumidor);
+    }
+
+}
+
+    class ImprimenaLinha implements Consumer<String>{
+
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    }
+
+
+    class ComparadorDeStringPorTamanho implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            if(s1.length() < s2.length())
+                return -1;
+            if(s1.length() > s2.length())
+                return 1;
+            return 0;
+        }
+    }
+
+

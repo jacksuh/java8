ff93f462099c8ee3893d59068804ef3ee6c0ee05
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-06-15 19:55:30 -0300

    Java8 Lambdas

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
     public static void main(String[] args) {
@@ -26,8 +27,25 @@ public class OrdenaStrings {
         });
 
         //Utilizando o Lambdas
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        //palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
+        palavras.sort(Comparator.comparing(s -> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
+
+
+        //exemplo antigo
+        Function<String, Integer> funcao = String::length;
+        Function<String, Integer> funcao2 = s -> s.length();
+
+        Comparator<String> comparador = Comparator.comparing(funcao);
+        palavras.sort(comparador);
+
+        System.out.println(palavras);
+
+        Consumer<String> impressor = System.out::println;
+        palavras.forEach(impressor);
+
+        palavras.forEach(System.out::println);
         //for(String p : palavras){
         //    System.out.println(p);
         //}
